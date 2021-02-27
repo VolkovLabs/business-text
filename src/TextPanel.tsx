281f1126e04016088ed848f555a69718e3089e5b
Marcus Olsson <marcus.olsson@hey.com>
2021-02-27 13:00:02 +0100

    Check for empty results
    Fixes #12

--- src/TextPanel.tsx
+++ src/TextPanel.tsx
@@ -1,15 +1,11 @@
-import React, { useState, useMemo } from 'react';
-import { PanelProps, DataFrameView, GrafanaTheme, textUtil } from '@grafana/data';
-import { Select, stylesFactory, useTheme } from '@grafana/ui';
+import React, { useState } from 'react';
+import { PanelProps, GrafanaTheme, SelectableValue } from '@grafana/data';
+import { InfoBox, Select, stylesFactory, useTheme } from '@grafana/ui';
 import { TextOptions } from 'types';
-import Handlebars from 'handlebars';
 
-import MarkdownIt from 'markdown-it';
+import { Text } from './Text';
 
-import { registerHelpers } from './helpers';
 import { css, cx } from 'emotion';
-
-registerHelpers(Handlebars);
 
 interface Props extends PanelProps<TextOptions> {}
 
@@ -19,9 +15,7 @@ export const TextPanel: React.FC<Props> = ({ options, data, width, height }) => 
   const theme = useTheme();
   const styles = getStyles(theme);
 
-  const { content } = options;
-
-  const onChangeFrame = (selectableValue: any) => {
+  const onChangeFrame = (selectableValue: SelectableValue<string>) => {
     const index = data.series.findIndex((frame) => frame.refId === selectableValue.value);
     setFrameIndex(index);
   };
@@ -31,24 +25,24 @@ export const TextPanel: React.FC<Props> = ({ options, data, width, height }) => 
     value: frame.refId,
   }));
 
+  if (data.series.length === 0) {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100%',
+        }}
+      >
+        <InfoBox severity="info" style={{ maxWidth: '500px' }}>
+          <p>{`Your query didn't return any results.`}</p>
+        </InfoBox>
+      </div>
+    );
+  }
+
   const frame = data.series[frameIndex];
-
-  /**
-   * By using useMemo here, we avoid having to compile and render the Markdown
-   * on every render.
-   */
-  const html = useMemo(() => {
-    return new DataFrameView(frame).toArray().map((row, key) => {
-      const md = new MarkdownIt({ html: true });
-
-      const template = Handlebars.compile(content ?? '');
-      const markdown = template(row);
-      const html = md.render(markdown);
-      const sanitizedHtml = textUtil.sanitize(html);
-
-      return <div key={key} className={styles.frame} dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />;
-    });
-  }, [content, frame, styles]);
 
   return (
     <div
@@ -60,7 +54,7 @@ export const TextPanel: React.FC<Props> = ({ options, data, width, height }) => 
         `
       )}
     >
-      {frame && <div style={{ flexGrow: 1, overflow: 'auto' }}>{html}</div>}
+      <Text frame={frame} content={options.content} />
 
       {data.series.length > 1 && (
         <div className={styles.frameSelect}>
@@ -78,50 +72,5 @@ const getStyles = stylesFactory((theme: GrafanaTheme) => ({
   `,
   frameSelect: css`
     padding: ${theme.spacing.sm};
-  `,
-  frame: css`
-    border-bottom: 1px solid ${theme.colors.panelBorder};
-    margin-bottom: 1rem;
-    padding: ${theme.spacing.sm};
-
-    &:last-child {
-      margin-bottom: 0;
-      border-bottom: 0;
-    }
-
-    li {
-      margin-left: ${theme.spacing.md};
-    }
-
-    table {
-      border-collapse: collapse;
-
-      th,
-      td {
-        padding: ${theme.spacing.xs} ${theme.spacing.sm};
-        border-top: 1px solid ${theme.colors.border2};
-        border-left: 1px solid ${theme.colors.border2};
-      }
-
-      th {
-        font-weight: ${theme.typography.weight.semibold};
-        background: ${theme.colors.bg2};
-      }
-
-      border-bottom: 1px solid ${theme.colors.border2};
-      border-right: 1px solid ${theme.colors.border2};
-    }
-
-    blockquote {
-      margin: ${theme.spacing.md} 0;
-      border-left: 5px solid ${theme.colors.border3};
-      padding: ${theme.spacing.sm};
-      padding-left: ${theme.spacing.md};
-
-      p {
-        font-size: ${theme.typography.size.base};
-        color: ${theme.colors.textSemiWeak};
-      }
-    }
   `,
 }));
