cdf606d3921c3715fa8d8ddad395afcca66955d8
Mikhail <mikhail@volkovlabs.io>
2023-02-19 09:16:15 -0500

    Refactor styles

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cx } from '@emotion/css';
+import { css, cx } from '@emotion/css';
 import { DataFrame } from '@grafana/data';
 import { Alert, useTheme2 } from '@grafana/ui';
 import { generateHtml } from '../../helpers';
@@ -33,7 +33,18 @@ export const Text: React.FC<Props> = ({ options, frame }) => {
    * Theme
    */
   const theme = useTheme2();
-  const styles = getStyles(theme, options.styles);
+  const styles = getStyles(theme);
+
+  /**
+   * Styles
+   */
+  const className = cx(
+    styles.highlight,
+    styles.frame,
+    css`
+      ${options.styles}
+    `
+  );
 
   try {
     /**
@@ -42,7 +53,7 @@ export const Text: React.FC<Props> = ({ options, frame }) => {
     if (!frame?.length) {
       return (
         <div
-          className={cx(styles.highlight, styles.frame)}
+          className={className}
           dangerouslySetInnerHTML={{ __html: generateHtml({}, options.defaultContent, options.helpers) }}
         />
       );
@@ -68,7 +79,7 @@ export const Text: React.FC<Props> = ({ options, frame }) => {
           {data.map((row, key) => (
             <div
               key={key}
-              className={cx(styles.highlight, styles.frame)}
+              className={className}
               dangerouslySetInnerHTML={{ __html: generateHtml(row, options.content, options.helpers) }}
             />
           ))}
@@ -81,7 +92,7 @@ export const Text: React.FC<Props> = ({ options, frame }) => {
      */
     return (
       <div
-        className={cx(styles.highlight, styles.frame)}
+        className={className}
         dangerouslySetInnerHTML={{ __html: generateHtml({ data }, options.content, options.helpers) }}
       />
     );
--- src/styles.ts
+++ src/styles.ts
@@ -5,63 +5,59 @@ import { GrafanaTheme2 } from '@grafana/data';
 /**
  * Styles
  */
-export const getStyles = (theme: GrafanaTheme2, custom: string) => {
+export const getStyles = (theme: GrafanaTheme2) => {
   /**
    * Frame
    */
-  const frame = custom
-    ? css`
-        ${custom}
-      `
-    : css`
-        border-bottom: 1px solid ${theme.colors.border.medium};
-        margin-bottom: 1rem;
-        padding: ${theme.spacing(1)};
+  const frame = css`
+    border-bottom: 1px solid ${theme.colors.border.medium};
+    margin-bottom: 1rem;
+    padding: ${theme.spacing(1)};
 
-        &:last-child {
-          margin-bottom: 0;
-          border-bottom: 0;
-        }
+    &:last-child {
+      margin-bottom: 0;
+      border-bottom: 0;
+    }
 
-        img {
-          max-width: 100%;
-        }
+    img {
+      max-width: 100%;
+    }
 
-        li {
-          margin-left: ${theme.spacing(2)};
-        }
+    li {
+      margin-left: ${theme.spacing(2)};
+    }
 
-        table {
-          border-collapse: collapse;
+    table {
+      border-collapse: collapse;
 
-          th,
-          td {
-            padding: ${theme.spacing(0.5)} ${theme.spacing(1)};
-            border-top: 1px solid ${theme.colors.border.medium};
-            border-left: 1px solid ${theme.colors.border.medium};
-          }
+      th,
+      td {
+        padding: ${theme.spacing(0.5)} ${theme.spacing(1)};
+        border-top: 1px solid ${theme.colors.border.medium};
+        border-left: 1px solid ${theme.colors.border.medium};
+      }
 
-          th {
-            font-weight: ${theme.typography.fontWeightMedium};
-            background: ${theme.colors.background.secondary};
-          }
+      th {
+        font-weight: ${theme.typography.fontWeightMedium};
+        background: ${theme.colors.background.secondary};
+      }
 
-          border-bottom: 1px solid ${theme.colors.border.medium};
-          border-right: 1px solid ${theme.colors.border.medium};
-        }
+      border-bottom: 1px solid ${theme.colors.border.medium};
+      border-right: 1px solid ${theme.colors.border.medium};
+    }
 
-        blockquote {
-          margin: ${theme.spacing(2)} 0;
-          border-left: 5px solid ${theme.colors.border.strong};
-          padding: ${theme.spacing(1)};
-          padding-left: ${theme.spacing(2)};
+    blockquote {
+      margin: ${theme.spacing(2)} 0;
+      border-left: 5px solid ${theme.colors.border.strong};
+      padding: ${theme.spacing(1)};
+      padding-left: ${theme.spacing(2)};
 
-          p {
-            font-size: ${theme.typography.body.fontSize};
-            color: ${theme.colors.text.secondary};
-          }
-        }
-      `;
+      p {
+        font-size: ${theme.typography.body.fontSize};
+        color: ${theme.colors.text.secondary};
+      }
+    }
+  `;
 
   /**
    * Highlight
