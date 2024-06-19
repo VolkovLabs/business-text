36c5c72f9be02d895f8d6c46dab3a7d1bc615b10
asimonok <sashasimonok@gmail.com>
2024-06-19 10:48:23 +0300

    Update tests to remove screenshot comparison and rename prod profile to main

--- test/panel.spec.ts
+++ test/panel.spec.ts
@@ -2,9 +2,12 @@ import { test, expect } from '@grafana/plugin-e2e';
 import { TEST_IDS } from '../src/constants/tests';
 
 test.describe('Volkovlabs Dynamictext Panel', () => {
-  test('Should display a Text', async ({ gotoDashboardPage, dashboardPage, page, grafanaVersion }) => {
+  test('Check grafana version', async ({ grafanaVersion }) => {
     console.log('Grafana version: ', grafanaVersion);
+    expect(grafanaVersion).toEqual(grafanaVersion);
+  });
 
+  test('Should display a Text', async ({ gotoDashboardPage, dashboardPage }) => {
     /**
      * Go To E2E dashboard
      * return dashboardPage
@@ -23,10 +26,35 @@ test.describe('Volkovlabs Dynamictext Panel', () => {
     await expect(dashboardPage.getPanelByTitle('Random Walk').locator.getByTestId(TEST_IDS.panel.root)).toBeVisible();
 
     /**
-     * Check and compare image
+     * Check content
      */
-    await expect(
-      dashboardPage.getPanelByTitle('Random Walk').locator.getByTestId(TEST_IDS.panel.root)
-    ).toHaveScreenshot('actual-screenshot.png');
+    await expect(dashboardPage.getPanelByTitle('Random Walk').locator.getByTestId(TEST_IDS.panel.root)).toHaveText(`{
+      "data": [
+        {
+          "time": 1713328742800,
+          "series": "71.606"
+        },
+        {
+          "time": 1713328762800,
+          "series": "71.617"
+        },
+        {
+          "time": 1713328782800,
+          "series": "72.013"
+        },
+        {
+          "time": 1713328802800,
+          "series": "71.865"
+        },
+        {
+          "time": 1713328822800,
+          "series": "72.165"
+        },
+        {
+          "time": 1713328842800,
+          "series": "71.712"
+        }
+      ]
+    }`);
   });
 });
