538bd995f81794d696b8cf29dd2a002da55b65f2
Vitali Pinchuk <vit.pinchuck@gmail.com>
2025-07-23 10:26:10 +0300

    update display logic for default content

--- src/module.ts
+++ src/module.ts
@@ -129,9 +129,7 @@ export const plugin = new PanelPlugin<PanelOptions>(TextPanel)
         defaultValue: DEFAULT_OPTIONS.defaultContent,
         editor: TextEditor,
         category: ['Content'],
-        showIf: (config) =>
-          config.editors.includes(EditorType.DEFAULT) ||
-          (!!config.defaultContent.length && config.defaultContent !== DEFAULT_OPTIONS.defaultContent),
+        showIf: (config) => config.editors.includes(EditorType.DEFAULT) || !!config.defaultContent.length,
       });
 
     /**
